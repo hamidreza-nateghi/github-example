5fd10e889fffd6cbc97b6aab3fcc5c6f15db30ea
Hamidreza <hamidrezatemp@gmail.com>
2021-04-11 14:18:20 +0430

    add check username

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { Input } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 
@@ -6,8 +7,37 @@ import styles from "../styles/Home.module.css";
 
 const { Search } = Input;
 
+const checkUsername = async (username) => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    if (response.ok) {
+      const user = await response.json();
+      return user;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  return false;
+};
+
 export default function Home() {
-  const handleSearch = (value) => console.log(value);
+  const router = useRouter();
+
+  const handleSearch = async (value) => {
+    try {
+      const user = await checkUsername(value);
+
+      if (user) {
+        router.push(`/profile/${value}`);
+      }
+
+      // TODO: username false
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className={styles.container}>
